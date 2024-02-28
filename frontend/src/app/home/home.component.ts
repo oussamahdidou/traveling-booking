7867002fd6d176d85bf1b90080c8839fbb2dbecc
oussama hdidou <oussamahdidou15@gmail.com>
2024-02-28 21:10:33 +0100

    caroussel

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -9,19 +9,6 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  slideConfig = {
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000
-  };
 
-  slides = [
-    { img: 'https://via.placeholder.com/600x400', alt: 'Placeholder 1' },
-    { img: 'https://via.placeholder.com/600x400', alt: 'Placeholder 2' },
-    { img: 'https://via.placeholder.com/600x400', alt: 'Placeholder 3' }
-  ];
 
 }
