6e1c47c6c878d75e25cb04e6b2924c91de11f95d
oussama hdidou <oussamahdidou15@gmail.com>
2024-02-28 19:42:42 +0100

    routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, SlickCarouselModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'client';
+  search: string = '';
 
 }
