c6a2ee7282ec5663cef9b48f69b6590edc8dd639
oussama hdidou <oussamahdidou15@gmail.com>
2024-02-29 23:36:42 +0100

    location

--- frontend/src/app/search/city/city.component.ts
+++ frontend/src/app/search/city/city.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-city',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './city.component.html',
   styleUrl: './city.component.css'
 })
 export class CityComponent {
+  movetonext() {
+    if (this.selectedIndex === this.images.length - 1) {
+      this.selectedIndex = 0;
+    }
+    else {
+      this.selectedIndex++;
+    }
+
+  }
+  selectedIndex: number = 0;
+  images: string[] = [
+    "https://assets.kerzner.com/api/public/content/22a13cd86bef48b28e9ff17642419a6c?v=abeba10a&t=w2880",
+    "https://www.dubai.it/fr/wp-content/uploads/sites/143/dubai-marina-hd.jpg",
+    "https://cf.bstatic.com/xdata/images/hotel/max1024x768/421989365.jpg?k=bb89cd96908bc50546bbeac16ca7c556cb3d29a198ec546b0dcc386ce508c005&o=&hp=1",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV3aA4zUsLFgoi9HYjMklH4GXlnFUySfq-9Q&usqp=CAU",
+    "https://reloadvisor.org/wp-content/uploads/2019/10/Dubai-ReloAdvisor.org_.jpg",
+  ];
 
 }
