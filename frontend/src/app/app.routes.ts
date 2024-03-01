9a1cda971e7e2467acbe3525dc8d4b7aadfba54e
oussama hdidou <oussamahdidou15@gmail.com>
2024-03-01 14:31:53 +0100

    place

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { EntrepriseComponent } from './entreprise/entreprise.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +17,10 @@ export const routes: Routes = [
     {
         path: 'search',
         loadChildren: () => import('./search/search.module').then((m) => m.SearchModule),
+    },
+    {
+        path: 'entreprise/:id',
+        component: EntrepriseComponent,
     },
     {
         path: '**',
---
+++ frontend/src/app/entreprise/entreprise.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entreprise',
+  standalone: true,
+  imports: [],
+  templateUrl: './entreprise.component.html',
+  styleUrl: './entreprise.component.css'
+})
+export class EntrepriseComponent {
+
+}
