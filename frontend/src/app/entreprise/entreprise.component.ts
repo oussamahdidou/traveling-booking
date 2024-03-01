6c682d0f5fcb99bbea26f1f19d32b8cccfcbaf8e
oussama hdidou <oussamahdidou15@gmail.com>
2024-03-01 17:16:07 +0100

    place

--- frontend/src/app/entreprise/entreprise.component.ts
+++ frontend/src/app/entreprise/entreprise.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './entreprise.component.css'
 })
 export class EntrepriseComponent {
+  constructor() { }
+  comments: any[] = [
+    { username: "user1", content: "This is the first comment." },
+    { username: "user2", content: "Nice work on this!" },
+    { username: "user3", content: "I have a question about the topic." },
+    { username: "user4", content: "Great explanation!" },
+    { username: "user5", content: "Thanks for sharing." }
+  ];
 
 }
