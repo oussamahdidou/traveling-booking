3e4a72da642138d6d9b8802b72916f15798a04a1
oussama hdidou <oussamahdidou15@gmail.com>
2024-03-06 21:24:43 +0100

    register

---
+++ frontend/src/app/auth/createpalce/createpalce.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createpalce',
+  standalone: true,
+  imports: [],
+  templateUrl: './createpalce.component.html',
+  styleUrl: './createpalce.component.css'
+})
+export class CreatepalceComponent {
+
+}
