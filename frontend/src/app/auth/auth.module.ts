662bcae4de6b5ef932c91aa2483d5d4b316f05d8
oussama hdidou <oussamahdidou15@gmail.com>
2024-03-06 21:35:22 +0100

    register

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { CreatepalceComponent } from './createpalce/createpalce.component';
 const routes: Routes = [
   {
     path: '',
@@ -11,6 +14,9 @@ const routes: Routes = [
 
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
+      { path: 'user', component: UserComponent },
+      { path: 'admin', component: AdminComponent },
+      { path: 'createplace', component: CreatepalceComponent }
     ],
   },
 ];
