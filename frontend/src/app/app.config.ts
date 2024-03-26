a702d631265d5943faddb7aab2d0f2173c038c1a
oussama hdidou <oussamahdidou15@gmail.com>
2024-03-26 02:05:58 +0000

    infinite scroll

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { AuthService } from './services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), AuthService],
+  providers: [provideRouter(routes), importProvidersFrom(BrowserModule)],
 };
--- frontend/src/app/search/city/city.component.ts
+++ frontend/src/app/search/city/city.component.ts
@@ -7,19 +7,22 @@ import { TruncatePipe } from '../../pipes/truncate.pipe';
 import { HeaderComponent } from '../../header/header.component';
 import { FooterComponent } from '../../footer/footer.component';
 import { HorisontalplaceholderComponent } from '../../placeholders/horisontalplaceholder/horisontalplaceholder.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-city',
   standalone: true,
   providers: [SearchService],
-  imports: [CommonModule, HttpClientModule, HorisontalplaceholderComponent, RouterModule, TruncatePipe, HeaderComponent, FooterComponent],
+  imports: [InfiniteScrollModule, CommonModule, HttpClientModule, HorisontalplaceholderComponent, RouterModule, TruncatePipe, HeaderComponent, FooterComponent],
   templateUrl: './city.component.html',
   styleUrl: './city.component.css'
 })
 export class CityComponent implements OnInit {
+
   places: any[] = [];
-  isLoading: boolean = false;
-  isLoadingTop: boolean = false;
+  isLoading: boolean = true;
+  isLoadingTop: boolean = true;
   top: any[] = [];
   constructor(private route: ActivatedRoute, private searchService: SearchService, private router: Router) { }
   ngOnInit(): void {
@@ -28,22 +31,25 @@ export class CityComponent implements OnInit {
       this.searchService.PlacesByCity(itemId).subscribe(
         response => {
           this.places = response;
-          this.isLoading = true;
+          this.isLoading = false;
         },
         error => {
-          this.isLoading = true;
+          this.isLoading = false;
           console.log(error)
         });
       this.searchService.TopPlacesByCity(itemId).subscribe(response => {
         console.log(response);
         console.log(itemId);
         this.top = response;
-        this.isLoadingTop = true;
+        this.isLoadingTop = false;
       })
 
     });
   }
-
+  onScroll() {
+    console.log("your in the end");
+    this.isLoading = false;
+  }
   movetonext(event: Event) {
     event.stopPropagation();
     if (this.selectedIndex === this.top.length - 1) {
